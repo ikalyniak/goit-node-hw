82fc58a7c8eee9118c8d6dd04f122c78b728d042
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-11-16 03:02:44 +0200

    pagination

--- controllers/contacts/getListContacts.js
+++ controllers/contacts/getListContacts.js
@@ -3,7 +3,15 @@ const successHelper = require('../../helpers/success');
 
 const getListContacts = async (req, res) => {
   const { _id } = req.user;
-  const result = await Contact.find({ owner: _id });
+  const { page, limit } = req.query;
+  // console.log(Number(page));// query has a type String!!!
+  // "Приведение к числу, унарный +" https://learn.javascript.ru/operators#privedenie-k-chislu-unarnyy
+  const skip = page * limit - limit;
+  const result = await Contact.find(
+    { owner: _id },
+    '_id name email phone favorite owner',
+    { skip, limit: +limit },
+  ).populate('owner', '_id email');
   successHelper.successfulResponse(res, result);
 };
 
