b94d208d9af47aa0be0f6691fd5d13ae50676d64
Ihor Kalyniak <kalyniak.ihor@gmail.com>
2021-12-04 19:57:26 +0200

    HW6

--- controllers/auth/signUp.js
+++ controllers/auth/signUp.js
@@ -2,7 +2,7 @@ const gravatar = require('gravatar');
 const { v4 } = require('uuid');
 
 const { User } = require('../../models');
-const { sendMail } = require('../../helpers/sendMail');
+const sendMail = require('../../helpers/auth/sendMail');
 const errorsHelper = require('../../helpers/auth/errors');
 const successHelper = require('../../helpers/auth/success');
 
@@ -19,7 +19,7 @@ const signUp = async (req, res) => {
   await newUser.save();
 
   // const text = `<a href="DOMAIN_NAME/api/auth/verify/${verificationToken}">Please confirm your email ${email}</a>`;
-  const text = `<a href="http://localhost:3000/api/auth/verify/${verificationToken}">Please confirm your email ${email}</a>`;
+  const text = `<p>Please confirm your email ${email}</p><a href="http://localhost:3000/api/auth/verify/${verificationToken}">Click here</a>`;
 
   const verificationEmail = {
     to: email,
@@ -32,3 +32,18 @@ const signUp = async (req, res) => {
 };
 
 module.exports = signUp;
+
+/* ---NOTE--------
+  subject: Mail delivery failed: returning message to sender
+  from: "Mail Delivery System" <Mailer-Daemon@meta.ua>
+
+  This message was created automatically by mail delivery software.
+
+  A message that you sent could not be delivered to one or more of its
+  recipients. This is a permanent error. The following address(es) failed:
+
+  ik.kiev.ua@gmail.com
+    host 192.168.1.4 [192.168.1.4]
+    SMTP error from remote mail server after end of data:
+    550 Message body: SPAM URL FOUND
+   */
